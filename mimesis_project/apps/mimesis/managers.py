6cfebbf4548db9d52ba0c94997e81196705b4cc8
lukeman <lukeman@gmail.com>
2010-11-18 01:16:08 -0500

    Allow both instance and model classes in Media.objects.for_model()

--- mimesis_project/apps/mimesis/managers.py
+++ mimesis_project/apps/mimesis/managers.py
@@ -1,14 +1,17 @@
 from django.db import models
-
 from django.contrib.contenttypes.models import ContentType
-
+from django.utils.encoding import force_unicode
 
 class MediaAssociationManager(models.Manager):
-    
+
     def for_model(self, model, content_type=None):
-        content_type = content_type or ContentType.objects.get_for_model(model)
-        objects = self.get_query_set().filter(
-            owner_content_type=content_type, 
-            owner_object_id=model.pk,
-        )
-        return objects
+        """
+        QuerySet for all media for a particular model (either an instance or
+        a class).
+        """
+        ct = content_type or ContentType.objects.get_for_model(model)
+        qs = self.get_query_set().filter(owner_content_type=ct)
+        if isinstance(model, models.Model):
+            qs = qs.filter(owner_object_id=force_unicode(model._get_pk_val()))
+        return qs
+
