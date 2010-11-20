cb080bf3089d508a211b11dd617f04bd0212c35e
lukeman <lukeman@gmail.com>
2010-11-19 19:25:04 -0500

    Absolute URLs and adding creator to media.

--- mimesis_project/apps/mimesis/models.py
+++ mimesis_project/apps/mimesis/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
@@ -12,12 +13,16 @@ from taggit.managers import TaggableManager
 class MediaBase(models.Model):
     
     title = models.CharField(max_length=150)
+    creator = models.ForeignKey(User)
     created = models.DateTimeField(default=datetime.datetime.now)
     
     tags = TaggableManager()
     
     class Meta:
         abstract = True
+    
+    def __unicode__(self):
+        return self.title
 
 
 class MediaAssociationBase(models.Model):
@@ -42,6 +47,9 @@ class Image(MediaBase):
 class ImageAssociation(MediaAssociationBase):
     
     image = models.ForeignKey(Image)
+    
+    def get_absolute_url(self):
+        return self.image.url
 
 
 class Audio(MediaBase):
@@ -52,6 +60,9 @@ class Audio(MediaBase):
 class AudioAssociation(MediaAssociationBase):
     
     audio = models.ForeignKey(Audio)
+    
+    def get_absolute_url(self):
+        return self.audio.url
 
 
 class Video(MediaBase):
@@ -62,4 +73,7 @@ class Video(MediaBase):
 class VideoAssociation(MediaAssociationBase):
     
     video = models.ForeignKey(Video)
+    
+    def get_absolute_url(self):
+        return self.video
 
