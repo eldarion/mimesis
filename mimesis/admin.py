04abead9e3bb725302cc87019a8d545afa272a4d
lukeman <lukeman@gmail.com>
2010-12-01 23:32:58 -0500

    Adding Audio to admin for testing.

--- mimesis/admin.py
+++ mimesis/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from mimesis.models import Image, ImageAssociation
+from mimesis.models import Audio, AudioAssociation
 
 admin.site.register(Image)
 admin.site.register(ImageAssociation)
+
+admin.site.register(Audio)
+admin.site.register(AudioAssociation)
