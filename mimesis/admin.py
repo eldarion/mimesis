4944b7518b9f238e1562f018caa85083c1d080b0
Patrick Altman <paltman@gmail.com>
2011-02-21 19:17:22 -0600

    whitespace and style nits

--- mimesis/admin.py
+++ mimesis/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 from mimesis.models import MediaUpload, MediaAssociation
 
+
 admin.site.register(MediaUpload)
 admin.site.register(MediaAssociation)
--- mimesis/managers.py
+++ mimesis/managers.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
 from django.utils.encoding import force_unicode
+
+from django.contrib.contenttypes.models import ContentType
+
 
 class MediaAssociationManager(models.Manager):
     
